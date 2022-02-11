dbb7e5dbc07870cafd4f40cabae03112e183d226
Kai Oishi <bokuchan.kai@gmail.com>
2022-02-11 19:33:02 +0900

    新規追加の＋ボタン追加

--- CustomPC/CustomListViewController.swift
+++ CustomPC/CustomListViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class CustomListViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var customTable: UITableView!
+    var addBarButtonItem: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "登録済み見積もり一覧"
+        addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped(_:)))
+        self.navigationItem.rightBarButtonItems = [addBarButtonItem]
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,6 +28,14 @@ class CustomListViewController: UIViewController ,UITableViewDelegate, UITableVi
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         cell.textLabel?.text = "あいうえお"
         return cell
+    }
+    
+    @objc func addBarButtonTapped(_ sender: UIBarButtonItem) {
+//        DispatchQueue.main.async {
+//            let storyboard = UIStoryboard(name: "NewCompanyFormViewController", bundle: nil)
+//            let nextVC = storyboard.instantiateViewController(identifier: "NewCompanyFormViewController")as! NewCompanyFormViewController
+//            self.navigationController?.pushViewController(nextVC, animated: true)
+//        }
     }
 }
 
