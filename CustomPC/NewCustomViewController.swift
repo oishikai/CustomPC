1480418676d564865fada187693655aa563dcfbc
Kai Oishi <bokuchan.kai@gmail.com>
2022-04-08 14:47:59 +0900

    パーツの登録画面をtebleviewに変更

--- CustomPC/NewCustomViewController.swift
+++ CustomPC/NewCustomViewController.swift
@@ -1,10 +1,26 @@
 import UIKit
 
-class NewCustomViewController: UIViewController{
+class NewCustomViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var selectTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "partsCell", for: indexPath)
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        cell.textLabel?.text = "abcd"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
 
