82622bc6a5bfb91b5e2b1f6a5cf244664563d89d
Kai Oishi <bokuchan.kai@gmail.com>
2022-04-10 14:16:15 +0900

    cellのラベルをパーツに合わせて変更

--- CustomPC/NewCustomViewController.swift
+++ CustomPC/NewCustomViewController.swift
@@ -4,18 +4,20 @@ class NewCustomViewController: UIViewController,UITableViewDelegate, UITableView
     
     @IBOutlet weak var selectTable: UITableView!
     
+    private let parts = ["CPU", "CPUクーラー", "メモリ", "グラボ/ビデオカード", "SSD", "HDD", "ケース", "電源", "ケースファン"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 9
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "partsCell", for: indexPath)
         cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = parts[indexPath.row]
         return cell
     }
     
