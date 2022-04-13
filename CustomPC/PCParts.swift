e20d9ed8895cb13e0d52c8c9b8bbe6a86b08bc93
Kai Oishi <bokuchan.kai@gmail.com>
2022-04-14 00:29:25 +0900

    PCParts型定義

---
+++ CustomPC/PCParts.swift
@@ -0,0 +1,13 @@
+//
+//  PCParts.swift
+//  CustomPC
+//
+//  Created by Kai on 2022/04/13.
+//
+
+import Foundation
+
+enum PcParts: String {
+    case cpu = "CPU"
+    case cpuCooler = "CPUクーラー"
+}
