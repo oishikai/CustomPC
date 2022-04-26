111b1eef962cbd86e9d575886e08ac52d7e23c6b
Kai Oishi <bokuchan.kai@gmail.com>
2022-04-26 23:31:37 +0900

    詳細画面StortBoad修正

--- CustomPC/ViewControllers/PartsDetailViewController.swift
+++ CustomPC/ViewControllers/PartsDetailViewController.swift
@@ -4,14 +4,21 @@ import Nuke
 class PartsDetailViewController: UIViewController{
         
     @IBOutlet weak var partsImageView: UIImageView!
+    @IBOutlet weak var makerLabel: UILabel!
     var pcparts: PcParts?
     
     @IBOutlet weak var contentsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        contentsView.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.0)
+        //R:220 G:220 B:220
+        if let parts = pcparts {
+            Nuke.loadImage(with: parts.image, into: partsImageView)
+        }
         
-        contentsView.backgroundColor = UIColor.gray
+        makerLabel.text = pcparts?.detailUrl
+        print("")
     }
     
 }
