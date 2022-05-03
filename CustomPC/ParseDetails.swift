c126023cca0a9b294c2057e7afb190b5e9151ca5
Kai Oishi <bokuchan.kai@gmail.com>
2022-05-03 22:54:09 +0900

    拡大画像複数取得メソッド追加

--- CustomPC/ParseDetails.swift
+++ CustomPC/ParseDetails.swift
@@ -31,4 +31,44 @@ class ParseDetails {
         }
     }
     
+    private static func fullscaleImage(detailUrl: String, completionHandler: @escaping (URL?) -> Void) -> Void{
+        var url:URL?
+        AF.request(detailUrl).responseString (encoding: String.Encoding.shiftJIS){ response in
+            if let html = response.value {
+                if let doc = try? HTML(html: html, encoding: String.Encoding.utf8) {
+                    for node in doc.css("img[src]") {
+                        if let strUrl = node["src"]{
+                            if (strUrl.contains("https://img1.kakaku.k-img.com/images/productimage/fullscale/")){
+                                url = URL(string: strUrl)!
+                                print("a")
+                                completionHandler(url)
+                                return
+                            }
+                        }
+                    }
+                    completionHandler(url)
+                }
+            }
+        }
+    }
+    
+    static func getAllFullscaleImages(detailUrl: String, completionHandler: @escaping (URL) -> Void) -> Void {
+        let imageViewUrl = detailUrl.replacingOccurrences(of: "?lid=pc_ksearch_kakakuitem", with: "") + "images/"
+        var i = 0
+        var varUrl = imageViewUrl
+        while true{
+            fullscaleImage(detailUrl: varUrl) { imageUrl in
+                if let url = imageUrl {
+                    completionHandler(url)
+                }
+            }
+            
+            if(i == 10){
+                break
+            }
+            
+            i += 1
+            varUrl = imageViewUrl + "page=ka_\(i)/"
+        }
+    }
 }
--- CustomPC/ViewControllers/PartsDetailViewController.swift
+++ CustomPC/ViewControllers/PartsDetailViewController.swift
@@ -6,22 +6,27 @@ class PartsDetailViewController: UIViewController{
     @IBOutlet weak var partsImageView: UIImageView!
     @IBOutlet weak var makerLabel: UILabel!
     var pcparts: PcParts?
-    
+    var fullScaleImage = [URL]()
     @IBOutlet weak var contentsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         contentsView.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.0)
         partsImageView.backgroundColor = UIColor.white
+        makerLabel.backgroundColor = UIColor.white
         //R:220 G:220 B:220
         if let parts = pcparts {
             ParseDetails.getFullscaleImages(detailUrl: parts.detailUrl) { url in
                 Nuke.loadImage(with: url, into: self.partsImageView)
             }
+            
+            ParseDetails.getAllFullscaleImages(detailUrl: parts.detailUrl) { ul in
+                self.fullScaleImage.append(ul)
+                print("")
+            }
+            print(self.fullScaleImage)
+            makerLabel.text = "    " + parts.maker
         }
-        
-        makerLabel.text = pcparts?.detailUrl
-        print("")
     }
     
 }
