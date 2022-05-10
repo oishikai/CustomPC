2d1129c545f9150578f581f093239acdf925015b
Kai Oishi <bokuchan.kai@gmail.com>
2022-05-11 00:25:09 +0900

    スライドショー実装完了

--- CustomPC/ViewControllers/PartsDetailViewController.swift
+++ CustomPC/ViewControllers/PartsDetailViewController.swift
@@ -3,11 +3,10 @@ import Nuke
 
 class PartsDetailViewController: UIViewController{
     
-    // @IBOutlet weak var partsImageView: UIImageView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var contentsView: UIView!
-    
+        
     var pcparts: PcParts?
     private var urls = [URL]()
     var currentIndex = 0
@@ -17,9 +16,6 @@ class PartsDetailViewController: UIViewController{
         collectionView.delegate = self
         collectionView.dataSource = self
         contentsView.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.0)
-        //        partsImageView.backgroundColor = UIColor.white
-        //        makerLabel.backgroundColor = UIColor.white
-        //R:220 G:220 B:220
         pageControl.backgroundColor = UIColor.gray
         if let parts = pcparts {
             let imageUrl = parts.detailUrl.replacingOccurrences(of: "?lid=pc_ksearch_kakakuitem", with: "") + "images/"
@@ -27,14 +23,21 @@ class PartsDetailViewController: UIViewController{
             ParseDetails.getFullscaleImages(detailUrl: imageUrl, urls: urls) { urls in
                 print(urls.count)
                 self.urls = urls
+                print("viewFrame=",self.collectionView.frame.size.width)
                 DispatchQueue.main.async {
                     self.pageControl.numberOfPages = urls.count
                     self.pageControl.reloadInputViews()
                     self.collectionView.reloadData()
+                    print("viewFrame=",self.collectionView.frame.size.width)
                 }
             }
-            //            makerLabel.text = "    " + parts.maker
         }
+        
+        let layout = UICollectionViewFlowLayout()
+          layout.scrollDirection = .horizontal
+//          layout.minimumInteritemSpacing = 0.0
+          layout.minimumLineSpacing = 0.0
+          collectionView.collectionViewLayout = layout
     }
     
     func getImageByUrl(imageUrl: URL) -> UIImage{
@@ -49,6 +52,7 @@ class PartsDetailViewController: UIViewController{
 }
 
 extension PartsDetailViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.urls.count
     }
@@ -60,14 +64,17 @@ extension PartsDetailViewController : UICollectionViewDelegate, UICollectionView
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let image = self.getImageByUrl(imageUrl: self.urls[indexPath.row])
-        print(image.size.width,image.size.height)
-        return CGSize(width: image.size.width, height: image.size.height)
+        let screen_width = UIScreen.main.bounds.size.width
+        return CGSize(width: screen_width, height: screen_width)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         currentIndex = Int(scrollView.contentOffset.x / collectionView.frame.size.width)
         pageControl.currentPage = currentIndex
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
 
