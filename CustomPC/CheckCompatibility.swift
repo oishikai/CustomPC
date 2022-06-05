e1df8ce2afb34b241d75a3f0bbabfbc99c1a334d
Kai Oishi <bokuchan.kai@gmail.com>
2022-06-05 21:23:34 +0900

    互換性確認クラス作成

---
+++ CustomPC/CheckCompatibility.swift
@@ -0,0 +1,15 @@
+//
+//  CheckCompatibility.swift
+//  CustomPC
+//
+//  Created by Kai on 2022/06/05.
+//
+
+import Foundation
+
+class CheckCompatibility {
+    
+    func compatibilityCpuMotherboard(cpu: PcParts, motherboard:PcParts) -> Bool {
+        
+    }
+}
