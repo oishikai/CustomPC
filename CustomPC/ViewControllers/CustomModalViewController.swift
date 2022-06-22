4230a53f0d8c69884930ddad916dd6011c671cec
Kai Oishi <bokuchan.kai@gmail.com>
2022-06-23 00:26:49 +0900

    モーダル表示用のviewController追加

---
+++ CustomPC/ViewControllers/CustomModalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomModalViewController.swift
+//  CustomPC
+//
+//  Created by Kai on 2022/06/23.
+//
+
+import UIKit
+
+class CustomModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
