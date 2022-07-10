6aa4bba1dd60821d63a7efea8386c13bb179df7a
Kai Oishi <bokuchan.kai@gmail.com>
2022-07-10 15:06:23 +0900

    StoredCustomViewController追加

---
+++ CustomPC/ViewControllers/StoredCustomViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SavedCustomViewController.swift
+//  CustomPC
+//
+//  Created by Kai on 2022/07/10.
+//
+
+import UIKit
+
+class StoredCustomViewController: UIViewController {
+    
+    var customTitle = ""
+    var customPrice = ""
+    
+    var storedParts:[PcParts] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
